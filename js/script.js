ea5e6744b0eb6768589d8863654fefb6edde6fdd
Talita Dovigo <88914279+TalitaGD@users.noreply.github.com>
2022-07-18 19:50:41 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ======================== typing animation ======================== */
+var typed = new Typed(".typing", {
+	strings:["", "Front-End Developer", "Designer UI e UX", "Designer Grafico", "Web Designer"],
+	typeSpeed: 100,
+	BackSpeed: 60,
+	loop: true
+})
